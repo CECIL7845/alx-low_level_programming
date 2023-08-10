dba4f49d00a39ae931e4edb1f2dddf50c3c5c113
Cecil Odonkor <odonkorcecil123@gmail.com>
2023-08-10 13:25:24 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,13 @@
 #include <unistd.h>
 
 /**
- * main - A program that print a new line to the standard error
- *
- * Return:1(Success)
- */
-
+* main - Entry point
+*
+* Return: Always 1 (Success)
+*/
 int main(void)
 {
-fput("ind that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-return (1);
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
