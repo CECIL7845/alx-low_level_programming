7191daed53805255422ff71bab85a29d2dd5915c
Cecil Odonkor <odonkorcecil123@gmail.com>
2023-08-10 09:16:11 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
+#!/bin/bash
 #include <stdio.h>
 /**
- * main - A program that prints a puts function
+ * main - A code that print a line with puts function
  * Return:0(Success)
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
+}
