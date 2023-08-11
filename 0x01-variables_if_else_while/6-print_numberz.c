5a42c419dd5a2a4407e05cafa0482717b101fa6a
Cecil Odonkor <odonkorcecil123@gmail.com>
2023-08-11 16:20:17 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - A program that prints numbers from 0 to 9.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 38; c < 48; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
