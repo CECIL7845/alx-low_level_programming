7af4c1edcda14ba5eb764cf632702402f1176f70
Cecil Odonkor <odonkorcecil123@gmail.com>
2023-09-01 14:01:58 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,50 +1,5 @@
 #include <stdio.h>
-#include "main.h"
-
-/**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the int converted from the string
- */
-int _atoi(char *s)
-{
-	int a, b, c, len, d, i;
-
-	a = 0;
-	b = 0;
-	c = 0;
-	len = 0;
-	d = 0;
-	i = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && d == 0)
-	{
-		if (s[a] == '-')
-			++b;
-
-		if (s[a] >= '0' && s[a] <= '9')
-		{
-			i = s[a] - '0';
-			if (b % 2)
-				i = -i;
-			c = c * 10 + i;
-			d = 1;
-			if (s[a + 1] < '0' || s[a + 1] > '9')
-				break;
-			d = 0;
-		}
-		a++;
-	}
-
-	if (d == 0)
-		return (0);
-
-	return (c);
-}
+#include <stdlib.h>
 
 /**
  * main - a program that multiplies two numbers
